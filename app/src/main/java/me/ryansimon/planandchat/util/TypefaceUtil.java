d9a2c9783e9adf4970bbb68faf8f782ac57de5a2
ryan-simon <linktothepast212@gmail.com>
2015-01-25 17:57:47 -0800

    added support for custom fonts

---
+++ app/src/main/java/me/ryansimon/planandchat/util/TypefaceUtil.java
@@ -0,0 +1,59 @@
+package me.ryansimon.planandchat.util;
+
+import android.content.Context;
+import android.graphics.Typeface;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author Ryan Simon
+ */
+public class TypefaceUtil {
+    private static final Map<String, Typeface> TYPEFACES = new HashMap<String, Typeface>(4);
+    private static String FONTS_DIR = "fonts/";
+
+    public static void setFontsDirectory(String directory) {
+        if (null != directory) {
+            FONTS_DIR = directory;
+            if (!FONTS_DIR.endsWith("/")) {
+                FONTS_DIR += "/";
+            }
+        } else {
+            FONTS_DIR = "";
+        }
+    }
+
+    public static Typeface getTypeface(final String name, final View view) {
+        return getTypeface(name, view.getContext());
+    }
+
+    public static Typeface getTypeface(final String name, final Context context) {
+        Typeface typeface = TYPEFACES.get(name);
+        if (typeface == null) {
+            typeface = Typeface.createFromAsset(context.getAssets(), FONTS_DIR + name);
+            TYPEFACES.put(name, typeface);
+        }
+        return typeface;
+    }
+
+    public static <V extends TextView> V setTypeface(final String name, final V view) {
+        if (view != null) {
+            view.setTypeface(getTypeface(name, view));
+        }
+        return view;
+    }
+
+    public static void setTypeface(final String name, final TextView... views) {
+        if (views == null || views.length == 0) {
+            return;
+        }
+
+        Typeface typeface = getTypeface(name, views[0]);
+        for (TextView view : views) {
+            view.setTypeface(typeface);
+        }
+    }
+}
---
+++ app/src/main/java/me/ryansimon/planandchat/widget/TypefaceTextView.java
@@ -0,0 +1,68 @@
+package me.ryansimon.planandchat.widget;
+
+import me.ryansimon.planandchat.R;
+import me.ryansimon.planandchat.util.TypefaceUtil;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+/**
+ * @author Ryan Simon
+ *  
+ * Use custom fonts with a Text View
+ */
+public class TypefaceTextView extends TextView {
+
+    public TypefaceTextView(Context context) {
+        this(context, null, R.attr.typefacedTextViewStyle);
+    }
+
+    public TypefaceTextView(Context context, AttributeSet attrs) {
+        this(context, attrs, R.attr.typefacedTextViewStyle);
+    }
+
+    public TypefaceTextView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+
+        if (!isInEditMode()) {
+            final TypedArray array = context.obtainStyledAttributes(attrs, R.styleable.TypefacedView, defStyle, R.style.TypefaceTextViewStyle);
+            if (array != null) {
+                final String typeface = array.getString(R.styleable.TypefacedView_typeface);
+                array.recycle();
+                if (null != typeface && typeface.length() > 0) {
+                    setCustomTypeface(typeface);
+                    return;
+                }
+            }
+            setOpenSansRegularTypeface();
+        }
+    }
+
+    public void setOpenSansLightTypeface() {
+        setCustomTypeface("OpenSans-Light");
+    }
+
+    public void setOpenSansRegularTypeface() {
+        setCustomTypeface("OpenSans-Regular");
+    }
+
+    public void setOpenSansSemiBoldTypeface() { setCustomTypeface("OpenSans-Semibold"); }
+
+    public void setOpenSansBoldTypeface() {
+        setCustomTypeface("OpenSans-Bold");
+    }
+
+    public void setOpenSansExtraBoldTypeface() {
+        setCustomTypeface("OpenSans-ExtraBold");
+    }
+
+    public void setOpenSansMediumTypeface() {
+        setCustomTypeface("OpenSans-SemiBold");
+    }
+
+    public void setCustomTypeface(String typeface) {
+        TypefaceUtil.setTypeface(typeface + ".ttf", this);
+    }
+}
